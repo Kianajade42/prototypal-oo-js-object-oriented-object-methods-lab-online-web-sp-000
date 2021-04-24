12889412f82d829d94b198964e91b0b6ab8a84b8
Kiana Long <kianajade42.kl@gmail.com>
2021-04-24 12:07:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,6 @@ BoardMember.prototype.approve=function(){
  BoardMember.prototype.doCharity=function(){
    return 'I like to help people.'
  }
+ BoardMember.prototype.releasePressStatement=function(){
+   return 'You will see great things from Scuber.'
+ }
