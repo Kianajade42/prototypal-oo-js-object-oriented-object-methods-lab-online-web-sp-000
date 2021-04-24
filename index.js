7a79db2bb9b37a6d5d96caa7d23d995e41b81ac8
Kiana Long <kianajade42.kl@gmail.com>
2021-04-24 12:04:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-class BoardMember
+class BoardMember{
   constructor(name,homeState,training){
     this.name=name
   this.homeState=homeState
   this.training=training
+}
 }
 BoardMember.prototype.veto=function(){
   return 'No,I must disagree'
