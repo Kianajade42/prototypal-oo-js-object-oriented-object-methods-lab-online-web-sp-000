87d14e688af1e4b5037ac59f3c3ac57a22897009
Kiana Long <kianajade42.kl@gmail.com>
2021-04-24 12:06:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ BoardMember.prototype.approve=function(){
   return 'You can do that!'
 }
  BoardMember.prototype.doCharity=function(){
-   return 'I like to help people'
+   return 'I like to help people.'
  }
